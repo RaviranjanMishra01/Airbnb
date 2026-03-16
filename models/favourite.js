3d62237e234ec76f5d1d030bc7fda257f1217dc3
raviranjan mishra <raviranjanmishra767@gmail.com>
2026-03-17 03:57:11 +0530

    favourite.js working

--- models/favourite.js
+++ models/favourite.js
@@ -1,27 +1,11 @@
-const { Getdb } = require("../utils/databaseUtil");
-
-module.exports = class Favourite {
-  constructor(houseId) {
-    this.houseId = houseId;
+const mongooes = require("mongoose")
+const fovouriteSchema = mongooes.Schema({
+  houseId:{
+    type: mongooes.Schema.Types.ObjectId,
+    ref:"Home",
+    require:true,
+    unique:true
   }
+});
 
-  save() {
-    const db = Getdb();
-    return db.collection('favourites').findOne({ houseId: this.houseId }).then(existingFav => {
-      if (!existingFav) {
-        return db.collection('favourites').insertOne(this);
-      }
-      return Promise.resolve();
-    })
-  }
-
-  static getFavourites() {
-    const db = Getdb();
-    return db.collection('favourites').find().toArray();
-  }
-
-  static deleteById(delHomeId) {
-    const db = Getdb();
-    return db.collection('favourites').deleteOne({ houseId: delHomeId });
-  }
-}
+module.exports = mongooes.model("Favourite",fovouriteSchema)
